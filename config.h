b48d12242901d3666bb10532f56d80c90cf9124a
Will Madden <w.a.madden@gmail.com>
2016-08-26 21:52:17 -0700

    Adjust config for Eugen's hat

--- config.h
+++ config.h
@@ -2,15 +2,15 @@
 #define RIM_LED_PIN 12   // which pin your pixels are connected to
 #define TOP_LED_PIN 6   // which pin your pixels are connected to
 
-#define RIM_LED_COUNT 93 // Number of LEDs around rim of hat
+#define RIM_LED_COUNT 60 // Number of LEDs around rim of hat
 #define TOP_LED_COUNT 36 // Number of LEDs on top of hat (i.e. around fan / goggles)
 
-#define RIM_PHYSICAL_CENTER 62
+#define RIM_PHYSICAL_CENTER 15
 
 #define COLOR_ORDER GRB
 
 // Audio config
-#define DC_OFFSET  380 // DC offset in mic signal
-#define MIC_PIN    9
+#define DC_OFFSET  500 // DC offset in mic signal
+#define MIC_PIN    A9
 #define MAX_AUDIO_LEVEL 80
 #define NOISE      0
