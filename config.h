a563b5d35d61e8c146cdafee62343f995d52528e
Will Madden <w.a.madden@gmail.com>
2016-08-28 15:59:03 -0700

    Update constants in `config.h`

--- config.h
+++ config.h
@@ -1,16 +1,17 @@
 // LED config
 #define RIM_LED_PIN 12   // which pin your pixels are connected to
-#define TOP_LED_PIN 6   // which pin your pixels are connected to
+#define TOP_LED_PIN 2   // which pin your pixels are connected to
 
 #define RIM_LED_COUNT 60 // Number of LEDs around rim of hat
-#define TOP_LED_COUNT 36 // Number of LEDs on top of hat (i.e. around fan / goggles)
+#define TOP_LED_COUNT 28 // Number of LEDs on top of hat (i.e. around fan / goggles)
 
-#define RIM_PHYSICAL_CENTER 15
+#define RIM_PHYSICAL_CENTER 13
+#define TOP_PHYSICAL_CENTER 21
 
 #define COLOR_ORDER GRB
 
 // Audio config
-#define DC_OFFSET  500 // DC offset in mic signal
+#define DC_OFFSET  550 // DC offset in mic signal
 #define MIC_PIN    A9
 #define MAX_AUDIO_LEVEL 80
-#define NOISE      0
+#define NOISE      20
